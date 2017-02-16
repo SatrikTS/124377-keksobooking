16f1e499589a4c54b71bfbd16d85dc58fe18cc0e
SatrikTS <satrik90@gmail.com>
2017-02-16 23:52:18 +0300

    Модуляция

--- js/form.js
+++ js/form.js
@@ -19,17 +19,22 @@ noticeFormAddress.required = true;
 
 // Изменения значений в полях формы - синхронизация
 // Синхронизация по времени
-
 var timeSelect = document.querySelector('#time');
 var timeoutSelect = document.querySelector('#timeout');
 var timeArray = ['twelve', 'thirteen', 'fourteen'];
 
-var guestArray = ['3guest', 'noguest'];
-var roomArray = ['1room', '2room', '100room'];
-
 // Синхронизация по количетсву метс для гостей
 var countRoom = document.querySelector('#room_number');
 var capacityGuest = document.querySelector('#capacity');
+var guestArray = ['3guest', 'noguest'];
+var roomCountArray = ['1room', '2room', '100room'];
+
+// Синхронизация по минимальной цене
+var housingType = document.querySelector('#type');
+var priceInput = document.querySelector('#price');
+var roomArray = ['Квартира', 'Лачуга', 'Дворец'];
+var minPrice = ['1000', '0', '10000'];
 
 window.synchronizeFields(timeSelect, timeoutSelect, timeArray, timeArray, 'value');
-window.synchronizeFields(countRoom, capacityGuest, roomArray, guestArray, 'value');
+window.synchronizeFields(countRoom, capacityGuest, roomCountArray, guestArray, 'value');
+window.synchronizeFields(housingType, priceInput, roomArray, minPrice, 'min');
--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,23 +1,7 @@
 'use strict';
 
-/*
-synchronize-fields.js — модуль, который экспортирует в глобальную область видимости функцию synchronizeFields.
-Функция принимает на вход пять параметров: в первых двух параметрах передаются DOM-элементы синхронизируемых полей.
-Следующие два параметра представляют собой два массива, которые содержат синхронизируемые значения.
-
-Например, если при выборе в первом поле значения с value а, во втором должно выбираться значение b (и наоборот),
-то массивы должны выглядеть как ['a'] и ['b'].
-Последний параметр содержит строку, с названием свойства второго объекта,
-которое нужно изменять при изменении первого (например, 'max' или 'value').
-
-Значения в массивах, которые передаются в функцию должны быть явно заданы в JS,
-а не посчитаны автоматически. Возьмите в качестве значений массивов те value,
-которые вы поставили для соответствующих записей в полях option в предыдущих заданиях.
-*/
-
-
 window.synchronizeFields = function (firstElement, secondElement, firstArr, secondArr, propertyName) {
-  var changeElement = function (eventt) {
+  var changeElement = function (event) {
     var target = event.target;
     var index;
 
@@ -33,37 +17,3 @@ window.synchronizeFields = function (firstElement, secondElement, firstArr, seco
   firstElement.addEventListener('change', changeElement);
   secondElement.addEventListener('change', changeElement);
 };
-
-/*
-// Синхронизация по количетсву метс для гостей
-var countRoom = document.querySelector('#room_number');
-var capacityGuest = document.querySelector('#capacity');
-
-capacityGuest.value = 'noguest';
-countRoom.addEventListener('change', function (event) {
-var oneRoom = event.target.value === '1room';
-event.preventDefault();
-if (oneRoom) {
-capacityGuest.value = 'noguest';
-} else {
-capacityGuest.value = '3guest';
-}
-});
-
-// Тип жилья и синхронизация с минимальной ценной
-var housingType = document.querySelector('#type');
-
-housingType.addEventListener('change', function (event) {
-  if (housingType.value === 'flat') {
-    noticeFormPrice.value = 1000;
-    noticeFormPrice.min = 1000;
-  } else if (housingType.value === 'shack') {
-    noticeFormPrice.value = 0;
-    noticeFormPrice.min = 0;
-  } else {
-    noticeFormPrice.value = 1000000;
-    noticeFormPrice.min = 10000;
-  }
-});
-
-*/
